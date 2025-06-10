c6fe494887352d24422c07de6e5d98d2f43d8a0a
Aaroh Sinha <aarohsinha.programming@gmail.com>
2025-06-10 13:12:09 -0400

    Till Cookies
    Completed Till Cookies. Will start with Sessions

---
+++ src/routes/users.mjs
@@ -0,0 +1,82 @@
+import { Router } from "express";
+import { query, validationResult, checkSchema, matchedData } from "express-validator"
+import { mockUsers } from "../utils/constants.js";
+import { createUserValidationSchema } from "../utils/validationSchemas.js";
+import { resolveIndexByUserId } from '../utils/middlewares.js';
+
+const router = Router(); // Router object
+// a good thing about routers is that it has all important functions such as get post as the normal app
+router.get('/api/users'
+  ,query('filter')
+  .isString()
+  .notEmpty().withMessage('Must Not be empty')
+  .isLength({min: 3, max: 10}).withMessage('Must be atlease 3 to 10 characters'),
+   (request, response) => {
+    /*
+    The query function above just perform the task no matter what
+    They dont throw any error. That error handling we need to do ourselves
+    */
+    const result = validationResult(request);
+
+    // this result will be an object. it will have an error property
+    // that errors witll be an array of differenet errors
+    const { 
+      query: {filter , value}
+     } = request;
+    if(!filter && !value) return response.status(201).send(mockUsers);
+    if(filter && value) return response.send(
+        mockUsers.filter((user) => user[filter].includes(value))
+    )
+});
+
+router.post('/api/users', 
+  checkSchema(createUserValidationSchema)
+  ,(request, response) => {
+    const result = validationResult(request);
+
+    // error handler
+    if(!result.isEmpty()){
+      return response.status(400).send({errors: result.array()}); // gives all validation errors in array
+    }
+
+    const data = matchedData(request);
+    console.log(data);
+    const {body} = request;
+    const newUser = {id:mockUsers[mockUsers.length - 1].id + 1, ...body};
+    mockUsers.push(newUser);
+    return response.status(201).send(newUser);
+})
+
+router.get('/api/users/:id', resolveIndexByUserId, (request, response) => {
+    const { findUserIndex } = request;
+    const findUser = mockUsers[findUserIndex];
+    if(!findUser){
+        return response.sendStatus(404);
+    }else{
+        return response.send(findUser);
+    }
+});
+
+// PUT
+router.put("/api/users/:id", resolveIndexByUserId, (request, response) => {
+  const { body, findUserIndex } = request; // findUserIndex is defined
+  mockUsers[findUserIndex] = { id: mockUsers[findUserIndex].id, ...body };
+  return response.sendStatus(200);
+});
+
+// PATCH
+router.patch("/api/users/:id", resolveIndexByUserId, (request, response) => {
+  const { body, findUserIndex } = request;
+  mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body };
+  return response.sendStatus(200);
+});
+
+// DELETE
+router.delete("/api/users/:id", resolveIndexByUserId, (request, response) => {
+  const { findUserIndex } = request;
+  mockUsers.splice(findUserIndex);
+  return response.sendStatus(200);
+});
+
+
+export default router;
---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+export const mockUsers = [
+    {id:1, username:"Anson", displayName: "Anson"},
+    {id:2, username:"Aaroh", displayName: "Aaroh"},
+    {id:3, username:"Bosch", displayName: "Bosch"},
+    {id:4, username:"Clara", displayName: "Clara"},
+    {id:5, username:"Derek", displayName: "Derek"},
+    {id:6, username:"Elina", displayName: "Elina"},
+    {id:7, username:"Farhan", displayName: "Farhan"},
+    {id:8, username:"Gia", displayName: "Gia"},
+    {id:9, username:"Hector", displayName: "Hector"},
+    {id:10, username:"Ivy", displayName: "Ivy"}
+];
